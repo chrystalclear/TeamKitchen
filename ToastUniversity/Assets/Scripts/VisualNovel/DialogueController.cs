a4b8ffa5a34e1a125431c05f1c76f22489ed0296
KirbiKaka <calvinlu@berkeley.edu>
2015-04-12 03:52:50 -0700

    Basic Dialogue Implemented

---
+++ ToastUniversity/Assets/Scripts/VisualNovel/DialogueController.cs
@@ -0,0 +1,137 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueController : MonoBehaviour {
+
+    public Texture dialogueBox;
+    public GUIStyle style;
+    public GUIStyle labelStyle;
+
+    private bool boxVisible = false;
+    private string label = "";
+    private string message = "";
+
+    // Character images during dialogue.
+    private string[] images;
+
+    private Color darkened = new Color(.5f, .5f, .5f);
+    private Color normal = new Color(1, 1, 1);
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonUp("Fire1")) {
+            if (boxVisible) {
+                SetBoxVisible(false);
+            }
+        }
+    }
+
+    void OnGUI () {
+        if (boxVisible) {
+            // Left edge of the camera
+            float leftEdgeX = Camera.main.ViewportToScreenPoint(new Vector3(0, 0, 0)).x;
+            int cameraWidth = Camera.main.pixelWidth;
+
+            // Draw character images. Main talkers are always drawn last.
+            if (images != null) {
+                for (int i = images.Length - 1; i >= 0; i--) {
+                    GUI.color = darkened;
+                    if (!IsMainTalker(images[i])) {
+                        GUI.DrawTexture(new Rect(leftEdgeX + (images.Length - (i + 1)) * cameraWidth / 11,
+                                                0,
+                                                cameraWidth,
+                                                Screen.height),
+                            GetTextureFromString(images[i]),
+                            ScaleMode.ScaleToFit);
+                    }
+                }
+                for (int i = images.Length - 1; i >= 0; i--) {
+                    GUI.color = normal;
+                    if (IsMainTalker(images[i])) {
+                        GUI.DrawTexture(new Rect(leftEdgeX + (images.Length - (i + 1)) * cameraWidth / 11,
+                                                0,
+                                                cameraWidth,
+                                                Screen.height),
+                            GetTextureFromString(images[i]),
+                            ScaleMode.ScaleToFit);
+                    }
+                }
+            }
+
+            // Draw box and text.
+            GUI.color = normal;
+            GUI.DrawTexture(new Rect(leftEdgeX,
+                                    Screen.height * 3 / 5,
+                                    cameraWidth,
+                                    Screen.height * 2 / 5),
+                            dialogueBox,
+                            ScaleMode.StretchToFill);
+            if (label != null) {
+                GUI.Label(new Rect(leftEdgeX + cameraWidth / 12 + cameraWidth / 32,
+                                  Screen.height * 3 / 5 + Screen.height / 32,
+                                  cameraWidth,
+                                  Screen.height / 3),
+                                  label,
+                                  labelStyle);
+                GUI.Label(new Rect(leftEdgeX + cameraWidth / 12,
+                                  Screen.height * 3 / 5 + Screen.height * 2 / 32,
+                                  cameraWidth * 6 / 8,
+                                  Screen.height / 3),
+                                  message,
+                                  style);
+            } else {
+                GUI.Label(new Rect(leftEdgeX + cameraWidth / 12,
+                                  Screen.height * 3 / 5 + Screen.height * 2 / 32,
+                                  cameraWidth * 6 / 8,
+                                  Screen.height / 3),
+                                  message,
+                                  style);
+            }
+        }
+    }
+
+    void OnMouseClick () {
+    }
+
+    public void ShowText (string l, string m, string[] left = null) {
+        SetBoxVisible(true);
+        label = l;
+        message = m;
+        images = left;
+    }
+
+    public void ShowText (string m, string[] left = null) {
+        SetBoxVisible(true);
+        label = null;
+        message = m;
+        images = left;
+    }
+
+    private bool IsMainTalker (string s) {
+        return s.Substring(s.Length - 2).Equals("_M");
+    }
+
+    private Texture GetTextureFromString (string s) {
+        if (s.Substring(s.Length - 2).Equals("_M")) {
+            s = s.Substring(0, s.Length - 2);
+        }
+        switch (s) {
+            case "Shaker_R":
+                return shaker_R;
+        }
+        return null;
+    }
+
+    public bool GetBoxVisible () {
+        return boxVisible;
+    }
+    public void SetBoxVisible (bool display) {
+        boxVisible = display;
+    }
+
+    public Texture shaker_R;
+}
---
+++ ToastUniversity/Assets/Scripts/VisualNovel/TestDialogue.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestDialogue : MonoBehaviour {
+
+    public GameObject dialogueHandler;
+
+    // The current step of dialogue. One array of messages.
+    private int step = 0;
+    // The current step in the array of messages.
+    private int arrayStep = 0;
+    // Finished with the current step. Stop sending messages until the next step.
+    private bool finished = false;
+
+    private string[] labels;
+    private string[] messages;
+    private string[][] images;
+
+    // Use this for initialization
+    void Start () {
+        LoadMessages(step);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (messages != null && arrayStep < messages.Length) {
+            if (!dialogueHandler.GetComponent<DialogueController>().GetBoxVisible()) {
+                dialogueHandler.GetComponent<DialogueController>().ShowText(labels[arrayStep], messages[arrayStep], images[arrayStep]);
+                arrayStep++;
+            }
+        } else if (arrayStep == messages.Length && !dialogueHandler.GetComponent<DialogueController>().GetBoxVisible()) {
+            // Load next level
+            // Application.LoadLevel("OpeningFight");
+        }
+    }
+
+    public void NextStep (int s = 10) {
+        step += s;
+        finished = false;
+        LoadMessages(step);
+    }
+
+    private void LoadMessages (int s) {
+        switch (s) {
+            case 0:
+                // Set this to the number of messages.
+                int num_messages = 11;
+                labels = new string[num_messages];
+                messages = new string[num_messages];
+                images = new string[num_messages][];
+                int i = 0;
+
+                labels[i] = null;
+                messages[i] = "*VRRRRRRMMMMMM-CRASH!*";
+
+                i = 1;
+                labels[i] = "Students";
+                messages[i] = "Hahahahaha!";
+
+                i = 2;
+                labels[i] = "Professor";
+                messages[i] = "Sigh... You know you have to put the lid on the blender first, right?";
+                images[i] = new string[1];
+                images[i][0] = "Shaker_R_M";
+
+                i = 3;
+                labels[i] = "You";
+                messages[i] = "...Oops...I...thought I did...";
+                images[i] = new string[1];
+                images[i][0] = "Shaker_R";
+
+                i = 4;
+                labels[i] = "You";
+                messages[i] = "Sorry... It won't happen again.";
+                images[i] = new string[1];
+                images[i][0] = "Shaker_R";
+
+                i = 5;
+                labels[i] = "Professor";
+                messages[i] = "Uh huh. Sounds just like what you said about the grill.";
+                images[i] = new string[1];
+                images[i][0] = "Shaker_R_M";
+
+                i = 6;
+                labels[i] = "Students";
+                messages[i] = "Man, what a waste of our tuition dollars.";
+                images[i] = new string[1];
+                images[i][0] = "Shaker_R";
+
+                i = 7;
+                labels[i] = "Students";
+                messages[i] = "I know. Our school starts a scholarship program and the first person they bring over is this klutz?";
+                images[i] = new string[1];
+                images[i][0] = "Shaker_R";
+
+                i = 8;
+                labels[i] = "Professor";
+                messages[i] = "Well, looks like it's 2 o'clock. You all can go. Don't forget about the quiz tomorrow!";
+                images[i] = new string[1];
+                images[i][0] = "Shaker_R_M";
+
+                i = 9;
+                labels[i] = "Professor";
+                messages[i] = "And YOU need to stay here and clean up this mess you've made.";
+                images[i] = new string[1];
+                images[i][0] = "Shaker_R_M";
+
+                i = 10;
+                labels[i] = "You";
+                messages[i] = "Shoot, I won't make it to my next class in time. I'm going to fall so behind...";
+
+                break;
+        }
+    }
+
+    public bool IsFinished () {
+        return finished;
+    }
+}
