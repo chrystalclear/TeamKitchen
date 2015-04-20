1e44b1359e72cabcaa3c7a244b08cde817ee10c0
zmirman <zmirman@yahoo.com>
2015-04-20 15:17:19 -0700

    got text parser to work

---
+++ ToastUniversity/Assets/Scripts/VisualNovel/DialogueController.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueController : MonoBehaviour {
+
+    public Texture dialogueBox;
+    public GUIStyle style;
+    public GUIStyle labelStyle;
+
+    private bool boxVisible = false;
+    private string label = "";
+    private string message = "";
+
+    // Character images during dialogue.
+    private string[] images;
+
+    private Color darkened = new Color(.5f, .5f, .5f);
+    private Color normal = new Color(1, 1, 1);
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonUp("Fire1")) {
+            if (boxVisible) {
+                SetBoxVisible(false);
+            }
+        }
+    }
+
+    void OnGUI () {
+        if (boxVisible) {
+            // Left edge of the camera
+            float leftEdgeX = Camera.main.ViewportToScreenPoint(new Vector3(0, 0, 0)).x;
+            int cameraWidth = (int)Camera.main.pixelWidth;
+
+            // Draw character images. Main talkers are always drawn last.
+            if (images != null) {
+                // If odd number of images, draw one in the center, and the rest off center
+                if (images.Length % 2 == 1) {
+                    int middle = images.Length / 2;
+                    for (int i = images.Length - 1; i >= 0; i--) {
+                        GUI.color = darkened;
+                        if (!IsMainTalker(images[i])) {
+                            GUI.DrawTexture(new Rect(leftEdgeX + (i - middle) * cameraWidth / 5,
+                                                    0,
+                                                    cameraWidth,
+                                                    Screen.height),
+                                            GetTextureFromString(images[i]),
+                                            ScaleMode.ScaleToFit);
+                        }
+                    }
+                    for (int i = images.Length - 1; i >= 0; i--) {
+                        GUI.color = normal;
+                        if (IsMainTalker(images[i])) {
+                            GUI.DrawTexture(new Rect(leftEdgeX + (i - middle) * cameraWidth / 5,
+                                                    0,
+                                                    cameraWidth,
+                                                    Screen.height),
+                                            GetTextureFromString(images[i]),
+                                            ScaleMode.ScaleToFit);
+                        }
+                    }
+                } else { // Else, draw them off center
+                    int middleL = images.Length / 2; // The middle image, rounded up. (e.g. indices 0, 1, 2, 3; and 4 / 2 = 2)
+                    for (int i = images.Length - 1; i >= 0; i--) {
+                        GUI.color = darkened;
+                        if (!IsMainTalker(images[i])) {
+                            if (i < middleL) {
+                                GUI.DrawTexture(new Rect(leftEdgeX - cameraWidth / 4 + ((i + 1) - middleL) * cameraWidth / 2,
+                                                        0,
+                                                        cameraWidth,
+                                                        Screen.height),
+                                    GetTextureFromString(images[i]),
+                                    ScaleMode.ScaleToFit);
+                            } else {
+                                GUI.DrawTexture(new Rect(leftEdgeX + cameraWidth / 4 + (i - middleL) * cameraWidth / 2,
+                                                        0,
+                                                        cameraWidth,
+                                                        Screen.height),
+                                    GetTextureFromString(images[i]),
+                                    ScaleMode.ScaleToFit);
+                            }
+                        }
+                    }
+                    for (int i = images.Length - 1; i >= 0; i--) {
+                        GUI.color = normal;
+                        if (IsMainTalker(images[i])) {
+                            if (i < middleL) {
+                                GUI.DrawTexture(new Rect(leftEdgeX - cameraWidth / 4 + ((i + 1) - middleL) * cameraWidth / 2,
+                                                        0,
+                                                        cameraWidth,
+                                                        Screen.height),
+                                    GetTextureFromString(images[i]),
+                                    ScaleMode.ScaleToFit);
+                            } else {
+                                GUI.DrawTexture(new Rect(leftEdgeX + cameraWidth / 4 + (i - middleL) * cameraWidth / 2,
+                                                        0,
+                                                        cameraWidth,
+                                                        Screen.height),
+                                    GetTextureFromString(images[i]),
+                                    ScaleMode.ScaleToFit);
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Draw box and text.
+            GUI.color = normal;
+            GUI.DrawTexture(new Rect(leftEdgeX,
+                                    Screen.height * 3 / 5,
+                                    cameraWidth,
+                                    Screen.height * 2 / 5),
+                            dialogueBox,
+                            ScaleMode.StretchToFill);
+            if (label != null) {
+                GUI.Label(new Rect(leftEdgeX + cameraWidth / 12 + cameraWidth / 32,
+                                  Screen.height * 3 / 5 + Screen.height / 32,
+                                  cameraWidth,
+                                  Screen.height / 3),
+                                  label,
+                                  labelStyle);
+                GUI.Label(new Rect(leftEdgeX + cameraWidth / 12,
+                                  Screen.height * 3 / 5 + Screen.height * 2 / 32,
+                                  cameraWidth * 6 / 8,
+                                  Screen.height / 3),
+                                  message,
+                                  style);
+            } else {
+                GUI.Label(new Rect(leftEdgeX + cameraWidth / 12,
+                                  Screen.height * 3 / 5 + Screen.height * 2 / 32,
+                                  cameraWidth * 6 / 8,
+                                  Screen.height / 3),
+                                  message,
+                                  style);
+            }
+        }
+    }
+
+    void OnMouseClick () {
+    }
+
+    public void ShowText (string l, string m, string[] left = null) {
+        SetBoxVisible(true);
+        label = l;
+        message = m;
+        images = left;
+    }
+
+    public void ShowText (string m, string[] left = null) {
+        SetBoxVisible(true);
+        label = null;
+        message = m;
+        images = left;
+    }
+
+    private bool IsMainTalker (string s) {
+        return s.Substring(s.Length - 2).Equals("_M");
+    }
+
+    private Texture GetTextureFromString (string s) {
+        if (s.Substring(s.Length - 2).Equals("_M")) {
+            s = s.Substring(0, s.Length - 2);
+        }
+        switch (s) {
+            case "Shaker_R":
+                return shaker_R;
+            case "Freezer":
+                return freezer;
+            case "Microwave":
+                return microwave;
+        }
+        return null;
+    }
+
+    public bool GetBoxVisible () {
+        return boxVisible;
+    }
+    public void SetBoxVisible (bool display) {
+        boxVisible = display;
+    }
+
+    public Texture shaker_R;
+    public Texture freezer;
+    public Texture microwave;
+
+}
---
+++ ToastUniversity/Assets/Scripts/VisualNovel/TestDialogue.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestDialogue : MonoBehaviour {
+
+    public GameObject dialogueHandler;
+
+    // The current step of dialogue. One array of messages.
+    private int step = 0;
+    // The current step in the array of messages.
+    private int arrayStep = 0;
+    // Finished with the current step. Stop sending messages until the next step.
+    private bool finished = false;
+
+    private string[] labels;
+    private string[] messages;
+    private string[][] images;
+
+	public string dialogFileName;
+
+    // Use this for initialization
+    void Start () {
+        LoadMessages(step);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (messages != null && arrayStep < messages.Length) {
+            if (!dialogueHandler.GetComponent<DialogueController>().GetBoxVisible()) {
+                dialogueHandler.GetComponent<DialogueController>().ShowText(labels[arrayStep], messages[arrayStep], images[arrayStep]);
+                arrayStep++;
+            }
+        } else if (arrayStep == messages.Length && !dialogueHandler.GetComponent<DialogueController>().GetBoxVisible()) {
+            // Load next level
+            // Application.LoadLevel("OpeningFight");
+        }
+    }
+
+    public void NextStep (int s = 10) {
+        step += s;
+        finished = false;
+        LoadMessages(step);
+    }
+
+    private void LoadMessages (int s) {
+		string[] lines = System.IO.File.ReadAllLines(dialogFileName);
+        // Set this to the number of messages.
+        int num_messages = 11;
+        labels = new string[num_messages];
+        messages = new string[num_messages];
+        images = new string[num_messages][];
+		int i = 0;
+		int j = 0;
+		int k;
+		while (i<lines.Length) {
+			labels [j] = lines[i++];
+			messages [j] = lines[i++];
+			int length;
+			int.TryParse (lines[i++], out length);
+			k=0;
+			if(length!=0){
+				images[j] = new string[length];
+				while(k<length){
+					images[j][k++]=lines[i++];
+				}
+			}
+			i++;
+			j++;
+
+		}
+    }
+
+    public bool IsFinished () {
+        return finished;
+    }
+}
